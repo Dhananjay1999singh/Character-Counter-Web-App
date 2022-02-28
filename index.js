31a9d75c8bb8198312612b6d296b87895290b84d
Dhananjay1999singh <dhananjay1999s@gmail.com>
2022-02-28 23:16:50 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const charVal = document.getElementById("textarea");
+let totalCount= document.getElementById("total-counter");
+let remainingCount = document.getElementById("remaining-counter");
+
+let char = 0;
+
+const updateCounter = () => {
+    userChar = charVal.value.length;
+
+    totalCount.innerText = userChar;
+
+    remainingCount.innerText = 150- userChar;
+};
+
+charVal.addEventListener('keyup', ()=> updateCounter());
+
+
+//  copy the text code
+const copyText = () => {
+   charVal.select();
+   charVal.setSelectionRange(0,9999); // for mobile version
+   navigator.clipboard.writeText(charVal.value); //
+};
+
+
+
